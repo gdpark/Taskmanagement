166bad2631b19b22563230de37131d6d3cf4024f
gdpark <gd.park5242@gmail.com>
2020-06-25 22:00:25 +0900

    table style test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,38 @@ import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Customer from './components/Customer';
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper'
+import TableContainer from '@material-ui/core/TableContainer';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+  
+})
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+
 
 const customer = [{
   'id':1,
@@ -33,9 +65,21 @@ const customer = [{
 
 class App extends Component{
   render(){
+
+    const {classes} = this.props;
+
     return(
-      <div>
-        {
+      <TableContainer component={Paper}>
+        <Table className = {classes.table} aria-label="simple table">
+          <TableHead>
+            <StyledTableCell align="center">번호</StyledTableCell>
+            <StyledTableCell align="center">이미지</StyledTableCell>
+            <StyledTableCell align="center">이름</StyledTableCell>
+            <StyledTableCell align="center">생년월일</StyledTableCell>
+            <StyledTableCell align="center">성별</StyledTableCell>
+            <StyledTableCell align="center">직업</StyledTableCell>
+          </TableHead>
+          <TableBody> {
           customer.map(c => {
             return(
               <Customer
@@ -50,9 +94,13 @@ class App extends Component{
             );
           })
         }
-     </div>
+        </TableBody>
+       
+        </Table>
+     
+     </TableContainer>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 
+const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
 
+  const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
 
 class Customer extends React.Component {
     render(){
         return(
-            <div>
-                <CustomerProfile name={this.props.name} id={this.props.id} image={this.props.image} birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}></CustomerProfile>
-                <CustomerInfo name={this.props.name} id={this.props.id} image={this.props.image} birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}></CustomerInfo>
-            </div>
+               <StyledTableRow key={this.props.id}>
+               <StyledTableCell  align="center">{this.props.id}</StyledTableCell >
+               <StyledTableCell  align="center"><img src={this.props.image} alt="profile"></img></StyledTableCell >
+               <StyledTableCell  align="center">{this.props.name}</StyledTableCell >
+               <StyledTableCell  align="center">{this.props.birthday}</StyledTableCell >
+               <StyledTableCell  align="center">{this.props.gender}</StyledTableCell >
+               <StyledTableCell  align="center">{this.props.job}</StyledTableCell >
+               </StyledTableRow>
+          
         );
     }
     
